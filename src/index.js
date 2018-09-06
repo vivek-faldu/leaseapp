2fe7fff0d238690e7f0c23bc1222eba64b14fb8f
vivek-faldu <vfaldu@asu.edu>
2018-09-06 15:23:01 -0700

    structure changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css'
+import App from './client/components/App';
+import './client/css/index.css'
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
