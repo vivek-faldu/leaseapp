c2628113c251aa52258c3341c361afa85ddd5355
vivek-faldu <vfaldu@asu.edu>
2018-09-06 15:06:16 -0700

    init

---
+++ src/server/index.js
@@ -0,0 +1,10 @@
+const app = require('express')();
+const http = require('http').Server(app);
+
+
+const PORT = process.env.PORT || 3231
+app.listen(PORT, ()=>{
+	console.log("Connected to port:" + PORT);
+});
+ 
+
