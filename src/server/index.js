930ad6db28d367b1096b4e4ea92962cf836285b8
vivek-faldu <vfaldu@asu.edu>
2018-09-07 16:24:51 -0700

    edited package.json for  windows support

--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,6 @@
 const app = require('express')();
 const http = require('http').Server(app);
 
-
 const PORT = process.env.PORT || 3231
 app.listen(PORT, ()=>{
 	console.log("Connected to port:" + PORT);
